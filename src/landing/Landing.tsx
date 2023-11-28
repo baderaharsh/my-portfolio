a943adb27b42fdb4b2cfde862cbd0bc08018a981
Harsh Badera <baderaharsh@gmail.com>
2023-11-28 18:24:04 +0530

    Align name text and animate it

--- src/landing/Landing.tsx
+++ src/landing/Landing.tsx
@@ -1,11 +1,13 @@
-import React, { Fragment } from "react";
+import React from "react";
 import classes from "./Landing.module.css";
 
 function Landing() {
-    return <Fragment>
-        <img src="/harsh.png" alt="me" className={classes.img}/>
-        <p className="">HARSH</p>
-    </Fragment>
+    return <div className={classes.divForImg}>
+        <div className={classes.innerDivForImage}>
+            <img src="/harsh.png" alt="me" className={classes.img} />
+            <h1 className="">HARSH</h1>
+        </div>
+    </div>
 }
 
 export default Landing;
