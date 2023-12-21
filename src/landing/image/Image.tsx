3c7a1651bec37cf393f95c280a1f5d2b616e340c
Harsh Badera <54855997+baderaharsh@users.noreply.github.com>
2023-12-22 00:32:01 +0530

    qa to master (#4)
    * Update image
    
    * Add css to make landing responsive
    
    * Complete landing responsive for smaller device screen
    
    * Respond to specific orientation only

--- src/landing/image/Image.tsx
+++ src/landing/image/Image.tsx
@@ -4,8 +4,7 @@ import classes from "./Image.module.css"
 function Image() {
     return <div className={classes.divForImg}>
         <div className={classes.innerDivForImage}>
-            <img src="/harsh.png" alt="me" className={classes.img} />
-            <h1 className={classes.name}>HARSH</h1>
+            <img src="/harsh.png" alt="me" className={classes.image} />
         </div>
     </div>
 }
