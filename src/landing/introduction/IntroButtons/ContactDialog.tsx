a2f9422d4ccf01f77e999b8e4a91c6f4486cfa2e
Harsh Badera <baderaharsh@gmail.com>
2023-12-22 00:18:47 +0530

    Complete landing responsive for smaller device screen

--- src/landing/introduction/IntroButtons/ContactDialog.tsx
+++ src/landing/introduction/IntroButtons/ContactDialog.tsx
@@ -1,6 +1,7 @@
 import { ContentCopy, Email, Phone } from "@mui/icons-material";
 import { Avatar, Button, Dialog, DialogActions, DialogTitle, IconButton, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
 import { ExternalUrls } from "../../../constants/ExternalUrls";
+import classes from "./ContactDialog.module.css";
 
 interface Props {
     open: boolean;
@@ -9,25 +10,33 @@ interface Props {
 
 export default function ContactDialog(props: Props) {
 
-    return <Dialog open={props.open} onClose={props.onClose}>
+    return <Dialog className={classes.modal} open={props.open} onClose={props.onClose} fullWidth>
         <DialogTitle>Reach out via: </DialogTitle>
         <List>
-            <ListItem key={ExternalUrls.EmailAddress} secondaryAction={<IconButton edge="end" onClick={() => navigator.clipboard.writeText(ExternalUrls.EmailAddress)}><ContentCopy /></IconButton>}>
-            <ListItemAvatar>
-                <Avatar>
-                    <Email />
-                </Avatar>
-            </ListItemAvatar>
+            <ListItem key={ExternalUrls.EmailAddress} secondaryAction={<IconButton edge="end" onClick={() => {
+                if (window.isSecureContext) {
+                    navigator.clipboard.writeText(ExternalUrls.EmailAddress)
+                }
+            }}><ContentCopy /></IconButton>}>
+                <ListItemAvatar>
+                    <Avatar>
+                        <Email />
+                    </Avatar>
+                </ListItemAvatar>
                 <ListItemText primary={ExternalUrls.EmailAddress} />
             </ListItem>
         </List>
         <List>
-            <ListItem key={ExternalUrls.Phone} secondaryAction={<IconButton edge="end" onClick={() => navigator.clipboard.writeText(ExternalUrls.Phone)}><ContentCopy /></IconButton>}>
-            <ListItemAvatar>
-                <Avatar>
-                    <Phone />
-                </Avatar>
-            </ListItemAvatar>
+            <ListItem key={ExternalUrls.Phone} secondaryAction={<IconButton edge="end" onClick={() => {
+                if (window.isSecureContext) {
+                    navigator.clipboard.writeText(ExternalUrls.Phone);
+                }
+            }}><ContentCopy /></IconButton>}>
+                <ListItemAvatar>
+                    <Avatar>
+                        <Phone />
+                    </Avatar>
+                </ListItemAvatar>
                 <ListItemText primary={ExternalUrls.Phone} />
             </ListItem>
         </List>
