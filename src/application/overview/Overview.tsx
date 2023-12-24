7d1daa0c20b37bf73b5cc72bab0357b31632eac7
Harsh Badera <baderaharsh@gmail.com>
2023-12-24 22:00:06 +0530

    Make experience overview card responsive

--- src/application/overview/Overview.tsx
+++ src/application/overview/Overview.tsx
@@ -5,10 +5,13 @@ import { Quantiphi, Rain, IBM } from "../../constants/Organization"
 export default function Overview() {
 
     return <div className={classes.overview}>
-        <div className={classes.innerOverview}>
-            <OverviewImage organization={Quantiphi} />
-            <OverviewImage organization={Rain} />
-            <OverviewImage organization={IBM} />
+        <div className={classes.overviewCard}>
+            <h1>Experience</h1>
+            <div className={classes.innerOverview}>
+                <OverviewImage organization={Quantiphi} />
+                <OverviewImage organization={Rain} />
+                <OverviewImage organization={IBM} />
+            </div>
         </div>
     </div>
 }
