a771c43841d41740c93b70d1617aca8e808d9cf0
Harsh Badera <baderaharsh@gmail.com>
2023-12-26 14:40:38 +0530

    Refactor org summary

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Main from "./application/Main"
 import './App.css';
 
 function App() {
+  console.log("This is app")
   return <Fragment>
     <Main />
   </Fragment>;
--- src/application/overview/Overview.tsx
+++ src/application/overview/Overview.tsx
@@ -1,16 +1,14 @@
 import classes from "./Overview.module.css";
+import json from "../../constants/fullTimeExperience.json";
 import OverviewImage from "./OverviewImage";
-import { Quantiphi, Rain, IBM } from "../../constants/Organization"
 
 export default function Overview() {
 
     return <div className={classes.overview}>
         <div className={classes.overviewCard}>
             <h2>Experience</h2>
-            <div className={classes.innerOverview}>
-                <OverviewImage organization={Quantiphi} />
-                <OverviewImage organization={Rain} />
-                <OverviewImage organization={IBM} />
+            <div className={classes.organizations}>
+                {json.organizations.map(org => <OverviewImage organization={org} />)}
             </div>
         </div>
     </div>
