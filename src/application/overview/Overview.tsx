c7860c6ddc1db62933d0fe48e041b027ffb6da5a
Harsh Badera <54855997+baderaharsh@users.noreply.github.com>
2024-01-28 18:35:15 +0530

    Update font settings (#13)

--- src/application/overview/Overview.tsx
+++ src/application/overview/Overview.tsx
@@ -6,7 +6,7 @@ export default function Overview() {
 
     return <div className={classes.overview}>
         <div className={classes.overviewCard}>
-            <h2>Experience</h2>
+            <h2>I have worked at...</h2>
             <div className={classes.organizations}>
                 {json.organizations.map(org => <OverviewImage key={org.id} organization={org} />)}
             </div>
--- src/application/skills/Skills.tsx
+++ src/application/skills/Skills.tsx
@@ -5,7 +5,7 @@ import Skill from "./Skill";
 export default function Skills() {
 
     return <div className={classes.skills}>
-        <div className={classes.header}><h1>Skills</h1></div>
+        <div className={classes.header}><h2>Skills</h2></div>
         <div className={classes.vl} />
         <div className={classes.skill}>
             {skillsJson.skills.map((skill) => <Skill key={skill.label} skill={skill} />)}
