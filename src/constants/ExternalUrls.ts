b4e8d2599f1d7bf6e9005de1a2641082d38a5475
Harsh Badera <baderaharsh@gmail.com>
2024-01-28 19:26:43 +0530

    Get CV URL from env

--- src/constants/ExternalUrls.ts
+++ src/constants/ExternalUrls.ts
@@ -5,6 +5,5 @@ export const enum ExternalUrls {
     Medium = "https://medium.com/@baderaharsh",
     Email = "mailto:baderaharsh@gmail.com",
     EmailAddress = "baderaharsh@gmail.com",
-    Phone = "+91-8446889029",
-    CV = "https://drive.google.com/uc?export=download&id=1--DeMb1UN4jLAFIyem3CqfQLMt9zuTha"
+    Phone = "+91-8446889029"
 }
